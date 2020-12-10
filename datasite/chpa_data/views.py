21754ca92a5337832f448bc9a26b5bc4a15e68ab
qewwc <33505447+liushutian1137@users.noreply.github.com>
2020-12-10 17:02:28 +0800

    Update views.py

--- datasite/chpa_data/views.py
+++ datasite/chpa_data/views.py
@@ -5,9 +5,9 @@ import pandas as pd
 # Create your views here.
 
 def connect_sql(sql):
-    conn = pymysql.connect(host='localhost', user='root', passwd='9004261137l', db='douban', charset="utf8",
+    conn = pymysql.connect(host='localhost', user='root', passwd='********', db='douban', charset="utf8",
                            use_unicode="True", port=3306)
-    # conn = pymysql.connect(host='49.232.138.121', user='root', passwd='9004261137l', db='douban', charset="utf8",
+    # conn = pymysql.connect(host='49.232.138.111', user='root', passwd='******', db='douban', charset="utf8",
     #                        use_unicode="True", port=3306)
     cursor = conn.cursor()
     cursor.execute(sql)
